6b3d0631749533b53d2ef261b79548182c70ad5f
Giovanni-Pascoli <daniele.scaffai.2003@calvino.edu.it>
2022-04-26 11:30:42 +0200

    Andato avanti con SQL

--- db/db.sql
+++ db/db.sql
@@ -8,3 +8,18 @@ CREATE TABLE Tappa(
 	id int PRIMARY KEY AUTO_INCREMENT,
     nome varchar(255)
 );
+
+CREATE TABLE Qr_Code(
+	link varchar(255) PRIMARY KEY,
+    id_tappa int REFERENCES Tappa(id) NOT NULL
+);
+
+CREATE TABLE Utente_Percorre_Tappa(
+    email varchar(255) REFERENCES Utente(email) NOT NULL,
+    id_tappa int REFERENCES Tappa(id) NOT NULL,
+    PRIMARY KEY (email, id_tappa)
+    data 
+);
+
+
+
