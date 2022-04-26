f780ec67dd53e071d9bf3842d2780c803349784b
Giovanni-Pascoli <daniele.scaffai.2003@calvino.edu.it>
2022-04-26 14:08:09 +0200

    Aggiornamento DB

--- db/db.sql
+++ db/db.sql
@@ -1,25 +1,45 @@
-CREATE TABLE Utente(
-    email varchar(255) PRIMARY KEY NOT NULL,
+CREATE DATABASE IF NOT EXISTS GenovaRoute;
+
+CREATE TABLE IF NOT EXISTS Utente(
+    email varchar(255) PRIMARY KEY,
     nome varchar(255) NOT NULL,
     cognome varchar(255) NOT NULL,
-    psw vrachan varchar(255) NOT NULL
+    psw varchar(255) NOT NULL 
 );
-CREATE TABLE Tappa(
+
+CREATE TABLE IF NOT EXISTS Tappa(
 	id int PRIMARY KEY AUTO_INCREMENT,
-    nome varchar(255)
+    nome varchar(255) NOT NULL
 );
 
-CREATE TABLE Qr_Code(
+CREATE TABLE IF NOT EXISTS Percorso(
+	id int PRIMARY KEY AUTO_INCREMENT,
+    nome varchar(255) NOT NULL,
+    descrizione varchar(255) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS Qr_Code(
 	link varchar(255) PRIMARY KEY,
-    id_tappa int REFERENCES Tappa(id) NOT NULL
+    id_tappa int REFERENCES Tappa(id)
 );
 
-CREATE TABLE Utente_Percorre_Tappa(
-    email varchar(255) REFERENCES Utente(email) NOT NULL,
-    id_tappa int REFERENCES Tappa(id) NOT NULL,
-    PRIMARY KEY (email, id_tappa)
-    data 
+CREATE TABLE IF NOT EXISTS Utente_Percorre_Tappa(
+    email varchar(255) REFERENCES Utente(email),
+    id_tappa int REFERENCES Tappa(id),
+    PRIMARY KEY (email, id_tappa),
+    data date NOT NULL
 );
+
+CREATE TABLE IF NOT EXISTS Tappa_Appartiene_Percorso(
+    id_tappa int REFERENCES Tappa(id),
+    id_percorso int REFERENCES Percorso(id),
+    PRIMARY KEY (id_tappa, id_percorso)
+);
+
+
+
+
+
 
 
 
