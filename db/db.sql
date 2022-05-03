a8e2af5b3c12375f1f6739d3631b063e56415d4f
Giovanni-Pascoli <daniele.scaffai.2003@calvino.edu.it>
2022-05-03 14:15:11 +0200

    riempito db

--- db/db.sql
+++ db/db.sql
@@ -9,7 +9,10 @@ CREATE TABLE IF NOT EXISTS Utente(
 
 CREATE TABLE IF NOT EXISTS Tappa(
 	id int PRIMARY KEY AUTO_INCREMENT,
-    nome varchar(255) NOT NULL
+    nome varchar(255) NOT NULL,
+    descrizione varchar(255) NOT NULL,
+    via varchar(255) NOT NULL,
+    città varchar(255)
 );
 
 CREATE TABLE IF NOT EXISTS Percorso(
@@ -36,13 +39,18 @@ CREATE TABLE IF NOT EXISTS Tappa_Appartiene_Percorso(
 );
 
 --fill table Percorso with data
-INSERT INTO Percorso (nome, descrizione) VALUES ('Monumenti di Genova', 'Percorso per i monumenti di Genova');
+INSERT INTO Percorso (nome, descrizione) VALUES ('Visita a Genova', 'Percorso per i monumenti, musei e ristoranti di Genova');
 INSERT INTO Percorso (nome, descrizione) VALUES ('Vicolata', 'Percorso per la vicolata');
 
 --fill table Tappa with data
-INSERT INTO Tappa (nome) VALUES ('Piazza de Ferrari');
-INSERT INTO Tappa (nome) VALUES ('Statua Colombo');
-INSERT INTO Tappa (nome) VALUES ('Statua Garibaldi');
+INSERT INTO Tappa (nome, descrizione, via, città) VALUES ('Arco della Vittoria', 'Arco della Vittoria della seconda guerra mondiale', 'Piazza della Vittoria', 'Genova');
+INSERT INTO Tappa (nome, descrizione, via, città) VALUES ('Ponte monumentale', 'Bellissimo ponte monumentale in centro città', 'Via XX Settembre', 'Genova');
+INSERT INTO Tappa (nome, descrizione, via, città) VALUES ('Fontana de Ferrari', 'Fontana bellissima in una bella piazza', 'Piazza de Ferrari', 'Genova');
+INSERT INTO Tappa (nome, descrizione, via, città) VALUES ('Cattedrale di San Lorenzo', 'Meravigliosa cattedrale', 'Piazza San Lorenzo', 'Genova');
+INSERT INTO Tappa (nome, descrizione, via, città) VALUES ('Ombre Rosse', 'Ottimo ristorante con piatti tipici genovesi', 'Vico degli Indoratori', 'Genova');
+INSERT INTO Tappa (nome, descrizione, via, città) VALUES ('Vascello Neptune', 'Arco della Vittoria della seconda guerra mondiale', 'Piazza della Vittoria', 'Genova');
+INSERT INTO Tappa (nome, descrizione, via, città) VALUES ('Arco della Vittoria', 'Arco della Vittoria della seconda guerra mondiale', 'Piazza della Vittoria', 'Genova');
+INSERT INTO Tappa (nome, descrizione, via, città) VALUES ('Arco della Vittoria', 'Arco della Vittoria della seconda guerra mondiale', 'Piazza della Vittoria', 'Genova');
 
 --fill table Tappa_Appartiene_Percorso with data
 INSERT INTO Tappa_Appartiene_Percorso (id_tappa, id_percorso) VALUES (1, 1);
