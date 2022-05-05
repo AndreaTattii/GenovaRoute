db5cc654e83179c9d58da53f9b9508eb627da484
DanInCode <daniele.scaffai.2003@calvino.edu.it>
2022-05-05 20:37:39 +0200

    Implementazione quarto qrcode in svelte

--- mobile/profilo/index.php
+++ mobile/profilo/index.php
@@ -69,7 +69,7 @@ if ($result = $connessione->query($sql)) {
             </div>
             <div class="col s-4" style="padding-top: 15px; ">
                 <center>
-                    <a class="navbar-brand" href="../scanner/index.php">
+                    <a class="navbar-brand" href="../scanner/QrCode/index.html">
                         <img src="../../img/icons/scannerizza.png">
                     </a>
                 </center>
