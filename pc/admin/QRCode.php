7b09e8c2ed19ec82f4ff60e6afb934a8573a0321
DanInCode <daniele.scaffai.2003@calvino.edu.it>
2022-05-14 19:21:33 +0200

    Sistemata pagina stampa QR Code

--- pc/admin/QRCode.php
+++ pc/admin/QRCode.php
@@ -1,13 +1,58 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="../../bootstrap/js/bootstrap.min.js"></script>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSS Personale-->
+    <link rel="stylesheet" href="../../css/style.css">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="../../bootstrap/css/bootstrap.min.css">
+
+    <!-- font -->
+    <link href='https://fonts.googleapis.com/css?family=Playfair Display' rel='stylesheet'>
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Amiri:ital,wght@1,400;1,700&display=swap" rel="stylesheet">
+
+    <title>Genova Route</title>
+    <link rel="icon" href="../../img/Admin.png" type="image/icon type">
+
+</head>
+
+<body>
+
+    <!-- NAVBAR -->
+    <nav class="navbar  navbar-expand-lg" style="background-color: #B30000;">
+        <div class="container">
+            <div class="col">
+                <a class="navbar-brand" style="font-family: 'Amiri', serif; color: white; font-weight: bold; text-align: center; " href="index.php">
+                    <h1>Genova Route Administration</h1>
+                </a>
+            </div>
+
+        </div>
+    </nav>
+
+    <!-- CORPO -->
+
 <?php    
     session_start();
     
-    echo "<h1>QR Code generato</h1><hr/>";
+    echo '<b><h1 style="text-align:center;">QR Code generato</h1></b><br><br><br><br><br><br>';
     
     //set it to writable location, a place for temp generated PNG files
-    $PNG_TEMP_DIR = dirname(__FILE__).DIRECTORY_SEPARATOR.'temp'.DIRECTORY_SEPARATOR;
+    $PNG_TEMP_DIR = dirname(__FILE__).DIRECTORY_SEPARATOR.'qrCodes'.DIRECTORY_SEPARATOR;
     
     //html PNG location prefix
-    $PNG_WEB_DIR = 'temp/';
+    $PNG_WEB_DIR = 'qrCodes/';
 
     include "qrlib.php";    
     
@@ -38,14 +83,16 @@
         QRcode::png($_SESSION['idPercorso'].'.'.$_SESSION['idTappa'], $filename, $errorCorrectionLevel, $matrixPointSize, 2);    
         
     } else {    
-    
+        echo 'errore';
         //default data  
         QRcode::png('PHP QR Code :)', $filename, $errorCorrectionLevel, $matrixPointSize, 2);    
         
     }    
         
     //display generated file
-    echo '<img src="'.$PNG_WEB_DIR.basename($filename).'" />';  
+    echo '<img style="  display: block;
+    margin-left: auto;
+    margin-right: auto;" src="'.$PNG_WEB_DIR.basename($filename).'" />';  
     
     //config form
     echo '<form action="QRCode.php" method="post" name="qr" id="qr">
@@ -57,4 +104,30 @@
         window.history.replaceState( null, null, window.location.href );
     }
 </script>
+<?php
+    for ($i = 0; $i < 15; $i++) {
+        echo "<br>";
+    }
+    ?>
+    <div class="footer-clean" style="background-color:white; border-top-color:#F0F0F0;  border-top-style: solid; border-top-width: 3px; margin-top: 40px; position:fixed; bottom:0px; width:100%;">
+        <footer>
+
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-4 ">
+                    </div>
+                    <div class="col-4"></div>
+                    <center>
+                        <p style="text-decoration: none; color:black">Partita Iva: 02070920992</p>
+                        <p>GenovaRoute ©</p>
+                    </center>
+                </div>
+            </div>
+    </div>
+    </footer>
+    </div>
+
+</body>
+
+</html>
     
