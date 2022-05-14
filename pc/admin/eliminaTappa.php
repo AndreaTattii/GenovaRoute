61c9f991c776201874c0cefc82e81c77dd57c6f2
andrea <andrea.tatti.2003@calvino.edu.it>
2022-05-14 19:06:41 +0200

    fine debug admin

--- pc/admin/eliminaTappa.php
+++ pc/admin/eliminaTappa.php
@@ -21,7 +21,7 @@
             FROM tappa_appartiene_percorso
             WHERE id_tappa = ".$idTappa."
         )";
-    if ($result = $connessione->query($sql)) {
+    if ($resultPercorso = $connessione->query($sql)) {
         if($resultPercorso->num_rows > 0) {
             while ($rowPercorso = $resultPercorso->fetch_assoc()) {
                 
@@ -29,22 +29,24 @@
                         WHERE id_tappa = ".$idTappa."
                         AND id_percorso = ".$rowPercorso['id']."
                 ";
-                echo $query;
-                if($result = $connessione->query($sql)) {   
-                    if($result->num_rows > 0){
+                if($result = $connessione->query($query)) {  
+                    
+                    if($resultPercorso->num_rows > 0){
                         $row = $result->fetch_assoc();
                         $ordineTappa = $row['ordine'];
-                    }
+                    } 
+                    
                     
                 } else {
                     echo "Errore nella query: " . $sql . "<br>" . $connessione->error;
                 }
 
                 $query = "UPDATE tappa_appartiene_percorso SET ordine = (ordine-1)
-                    WHERE ordine > $ordineTappa
+                    WHERE ordine >= $ordineTappa
                     AND id_percorso = ".$rowPercorso['id']."
                 ";
-                if ($result = $connessione->query($sql)) {
+                echo $query;
+                if ($result = $connessione->query($query)) {
             
             
                 } else {
