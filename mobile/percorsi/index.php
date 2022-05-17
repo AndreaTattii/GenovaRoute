79cbee3683be8a5ee8e00d8f0076791e58676baf
andrea <andrea.tatti.2003@calvino.edu.it>
2022-05-17 20:48:57 +0200

    GrovaGO

--- mobile/percorsi/index.php
+++ mobile/percorsi/index.php
@@ -69,7 +69,7 @@ session_start();
         <div class="row justify-content-center align-items-center" style="background-color: #B30000;  padding-top: 10px; height:60px">
 
             <div class="col ">
-                <h1 style="font-family: 'Amiri', serif; color: white; font-weight: bold; text-align: center;">GrovaGo</h1>
+                <h1 style="font-family: 'Amiri', serif; color: white; font-weight: bold; text-align: center;">GrovaGO</h1>
             </div>
         </div>
     </div>
