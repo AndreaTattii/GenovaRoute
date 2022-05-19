5db37200ad343d70e91748da0e337e505b33dc3e
andrea tatti <andrea.tatti.2003@calvino.edu.it>
2022-05-19 13:10:14 +0200

    citta italiane ancora da cancellar duplicate

--- index.php
+++ index.php
@@ -142,7 +142,7 @@ body {
     <nav class="navbar  navbar-expand-lg " style="background-color: #B30000;">
         <div class="container p-2">
             <a class="navbar-brand" style="font-family: 'Amiri', serif; color: white; font-weight: bold;" href="./">
-                <h1>Genova Route</h1>
+                <h1>GrovaGO</h1>
             </a>
         </div>
     </nav>
