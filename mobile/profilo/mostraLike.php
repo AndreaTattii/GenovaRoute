4b7b39067c6acd4c628457f469745e82f3951d53
DanInCode <daniele.scaffai.2003@calvino.edu.it>
2022-05-24 18:13:31 +0200

    Backend visualizzazione stats su profilo

--- mobile/profilo/mostraLike.php
+++ mobile/profilo/mostraLike.php
@@ -12,9 +12,29 @@
         echo "Errore: ".$connessione->error;
     }
     $email=$_POST['email'];
-    //$sql = "";
-    //$result = $connessione->query($sql);
-
-
-    //header("Location: ../percorsi/index.php");
+    $sql="SELECT * FROM utente_percorre_tappa, tappa WHERE email = '" . $email . "' AND tappa.id=id_tappa AND piace=1";
+    $result = $connessione->query($sql);
+    if ($result->num_rows > 0) {
+        while($row = $result->fetch_assoc()) {
+            echo   "<div class='row'>
+                        <div class='col-sm-12'>
+                            <div class='card'>
+                                <div class='card-body'>
+                                    <h5 class='card-title'>".$row['nome']."</h5>
+                                    <p class='card-text'>".$row['descrizione']."</p>
+                                    <a href='../../percorso/mostraPercorso.php?id=".$row['id']."' class='btn btn-primary'>Vai alla tappa</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>";
+        }
+        echo"<br><br><br><br>";
+    } else {
+        if($_SESSION['email']==$email){
+            echo "Non hai messo mi piace a nessuna tappa";
+        }
+        else{
+            echo "Non ha messo mi piace a nessuna tappa";
+        }
+    }
 ?>
