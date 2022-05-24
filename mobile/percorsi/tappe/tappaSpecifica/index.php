e64958432c6e7f15f36832d170841c828f62eff9
DanInCode <daniele.scaffai.2003@calvino.edu.it>
2022-05-24 22:34:38 +0200

    Cambiato titoletto tappa

--- mobile/percorsi/tappe/tappaSpecifica/index.php
+++ mobile/percorsi/tappe/tappaSpecifica/index.php
@@ -71,7 +71,7 @@ if ($result = $connessione->query($sql)) {
     }
 }
 
-// CONTROLLO SE UTENTE HA MESSO MI PIACE A TAPPAS
+// CONTROLLO SE UTENTE HA MESSO MI PIACE A TAPPA
 $sql = "SELECT * FROM utente_percorre_tappa
         WHERE email = '" . $_SESSION['email'] . "'
         AND id_tappa = ".$id."
@@ -178,7 +178,7 @@ if ($result = $connessione->query($sql)) {
                 </a>
             </div>
             <div class="col-8">
-                <h1 style="font-family: 'Amiri', serif; color: white; font-weight: bold; text-align: center;  font-size: 20px;"><?php echo $nome;  ?></h1>
+                <h1 style=" color: white; font-weight: bold; text-align: center;  font-size: 20px;"><?php echo $_SESSION['ordineTappa']+1;  echo '. '.$nome; ?></h1>
             </div>
             <div class="col-2">
                 <center>
