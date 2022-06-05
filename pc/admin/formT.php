1d41183c52260661c72a65b34936a5df562accb4
andrea <andrea.tatti.2003@calvino.edu.it>
2022-06-05 20:29:49 +0200

    inizio categorie in admin

--- pc/admin/formT.php
+++ pc/admin/formT.php
@@ -1,12 +1,21 @@
-<?php session_start();
-
+<?php 
+session_start();
+$_SESSION['arrivoDaCercaPerPercorsi'] = true;
+$host = "127.0.0.1";
+$user = "root";
+$pass = "";
+$database = "genovaroute";
+$connessione = new mysqli($host, $user, $pass, $database);
+if ($connessione === false) {
+    die("Errore: " . $connessione->connect_error);
+}
 ?>
 <!doctype html>
 <html lang="en">
 
 <head>
     <script src="https://unpkg.com/leaflet@1.6.0/dist/leaflet.js"></script>
-    <link href="https://unpkg.com/leaflet@1.6.0/dist/leaflet.css" rel="stylesheet"/>
+    <link href="https://unpkg.com/leaflet@1.6.0/dist/leaflet.css" rel="stylesheet" />
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <script src="../../bootstrap/js/bootstrap.min.js"></script>
     <!-- Required meta tags -->
@@ -29,6 +38,8 @@
     <title>Genova Route</title>
     <link rel="icon" href="../../img/Admin.png" type="image/icon type">
 
+
+    
 </head>
 
 <body class="d-flex flex-column min-vh-100">
@@ -63,7 +74,35 @@
             </div>
             <div class="form-group">
                 <label for="descrizione">Descrizione</label>
-                <textarea class="form-control" placeholder="Inserisci descrizione" name="nuovaDescrizione" maxlength="2000"  required></textarea>
+                <textarea class="form-control" placeholder="Inserisci descrizione" name="nuovaDescrizione" maxlength="2000" required></textarea>
+            </div>
+            <div class="form-group">
+                <label for="descrizione">Categoria</label>
+                <div class="row">
+
+                    <?php
+                        $sql = "
+                            SELECT *
+                            FROM categoria
+                        ";
+                        if($result = $connessione->query($sql)){
+                            while($row = $result->fetch_assoc()){
+                                
+                            }
+                        }else{
+                            echo "La query".$sql."è errata:".$connessione->error;
+                        }
+                    ?>
+                    <div class="col-sm-1">
+                        <div class="form-check">
+                                <input class="form-check-input" type="radio" name="sezione" id="flexRadioDefault2" value="antipasti" checked>
+                                <label class="form-check-label" for="flexRadioDefault2">
+                                    Antipasti
+                                </label>
+                        </div>
+                    </div>
+                    
+                </div>
             </div>
             <div class="form-group">
                 <label for="immagine">Immagine 1</label>
@@ -92,10 +131,12 @@
                 <input type="text" class="form-control" id="città" name="città" placeholder="Inserisci la città della tappa" maxlength="30" required>
             </div>
             <div class="row">
-        
+
                 <div class="col-6">
                     <h1>Seleziona le coordinate</h1>
-                    <strong><p style="color:red" id="controlla">Inserisci prima una città valida</p></strong>
+                    <strong>
+                        <p style="color:red" id="controlla">Inserisci prima una città valida</p>
+                    </strong>
                     <div id="osm-map"></div>
                 </div>
                 <div class="col-6" style="padding-top:225px">
@@ -112,7 +153,7 @@
 
             <button type="submit" class="btn btn-primary" style="margin-top: 15px; background-color:#B30000; border-color:#B30000;">Inserisci</button>
         </form>
-    </div>  <!-- fine container -->
+    </div> <!-- fine container -->
     <!-- stampa tanti br -->
     <?php
     for ($i = 0; $i < 2; $i++) {
@@ -137,91 +178,94 @@
     </div>
     </footer>
     </div>
-        <script>
-            element = document.getElementById('osm-map');
-            element.style = 'height:'.concat(500, 'px;');
-            var map = L.map(element);
-            L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {}).addTo(map);
-            var Icon1 = L.icon({
-                            iconUrl: '../../img/icons/marker.png',
-                            iconSize:     [40, 40],
-                        });
-            var IconB = L.icon({
-                iconUrl: '../../img/GB.png',
-                iconSize:     [40, 40]
+    <script>
+        element = document.getElementById('osm-map');
+        element.style = 'height:'.concat(500, 'px;');
+        var map = L.map(element);
+        L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {}).addTo(map);
+        var Icon1 = L.icon({
+            iconUrl: '../../img/icons/marker.png',
+            iconSize: [40, 40],
+        });
+        var IconB = L.icon({
+            iconUrl: '../../img/GB.png',
+            iconSize: [40, 40]
+        });
+        //when the user types in the input with name città, send a query to coordinateCitta.php with ajax jquery with json dataType
+        $("input[name='città']").keyup(function() {
+            var città = $("input[name='città']").val()
+            $.ajax({
+                url: 'coordinateCitta.php',
+                type: 'POST',
+                data: {
+                    città: città
+                },
+                dataType: "json",
+                success: function(data) {
+                    $("#controlla").hide();
+                    //change the view of the map to the coordinates of the city
+                    map.setView([data.x, data.y], 15);
+                    //set bounds of the map to the entire world
+                    map.setMaxBounds([
+                        [90, 180],
+                        [-90, -180]
+                    ]);
+                    //if the query is successful, the coordinates are shown in the map
+                    //map.setView([data.lat, data.lon], 13);
+                },
+                error: function() {
+                    $("#controlla").show();
+                }
             });
-            //when the user types in the input with name città, send a query to coordinateCitta.php with ajax jquery with json dataType
-            $("input[name='città']").keyup(function() {
-                var città = $("input[name='città']").val()
-                $.ajax({
-                    url: 'coordinateCitta.php',
-                    type: 'POST',
-                    data: {
-                        città: città
-                    },
-                    dataType: "json",
-                    success: function(data) {
-                        $("#controlla").hide();
-                        //change the view of the map to the coordinates of the city
-                        map.setView([data.x, data.y], 15);
-                        //set bounds of the map to the entire world
-                        map.setMaxBounds([
-                            [90, 180],
-                            [-90, -180]
-                        ]);
-                        //if the query is successful, the coordinates are shown in the map
-                        //map.setView([data.lat, data.lon], 13);
-                    },
-                    error : function () {
-                        $("#controlla").show();
-                    }
-                });
-            });
+        });
 
-            var markerGroup = L.layerGroup().addTo(map); 
-            var markerGroupCitta = L.layerGroup().addTo(map);       
+        var markerGroup = L.layerGroup().addTo(map);
+        var markerGroupCitta = L.layerGroup().addTo(map);
 
-            
-           //when the user clicks on the map, create a layer with a draggable marker and prevent to create multiple markers
-            map.on('click', function(e) {
-                //delete all markers from marker group
-                markerGroup.clearLayers();
-                marker = new L.marker(e.latlng, {draggable: true, icon: IconB}).addTo(markerGroup);
+
+        //when the user clicks on the map, create a layer with a draggable marker and prevent to create multiple markers
+        map.on('click', function(e) {
+            //delete all markers from marker group
+            markerGroup.clearLayers();
+            marker = new L.marker(e.latlng, {
+                draggable: true,
+                icon: IconB
+            }).addTo(markerGroup);
+            var coord = marker.getLatLng();
+            document.getElementById('latitudine').value = coord.lat;
+            document.getElementById('longitudine').value = coord.lng;
+
+            marker.on('dragend', function(e) {
                 var coord = marker.getLatLng();
                 document.getElementById('latitudine').value = coord.lat;
                 document.getElementById('longitudine').value = coord.lng;
-
-                marker.on('dragend', function(e) {
-                    var coord = marker.getLatLng();
-                    document.getElementById('latitudine').value = coord.lat;
-                    document.getElementById('longitudine').value = coord.lng;
-                });
             });
+        });
 
-            $("input[name='città']").keyup(function() {
-                var città = $("input[name='città']").val()
-                $.ajax({
-                    url: 'stampaMarkerCitta.php',
-                    type: 'POST',
-                    data: {
-                        città: città
-                    },
-                    dataType: "json",
-                    success: function(data) {
-                        //print all the markers in the mark using the data from the query
-                        //alert((data).length);
-                        for (var i = 0; i < 10; i++) {
-                            //crea un marker concatenando la variabile i con data.lat e data.lon
-                            marker = new L.marker([data.lat, data.lon], {icon: Icon1}).addTo(markerGroupCitta).bindPopup(data.nome);
-                        }
-                    },
-                    error : function () {
-
+        $("input[name='città']").keyup(function() {
+            var città = $("input[name='città']").val()
+            $.ajax({
+                url: 'stampaMarkerCitta.php',
+                type: 'POST',
+                data: {
+                    città: città
+                },
+                dataType: "json",
+                success: function(data) {
+                    //print all the markers in the mark using the data from the query
+                    //alert((data).length);
+                    for (var i = 0; i < 10; i++) {
+                        //crea un marker concatenando la variabile i con data.lat e data.lon
+                        marker = new L.marker([data.lat, data.lon], {
+                            icon: Icon1
+                        }).addTo(markerGroupCitta).bindPopup(data.nome);
                     }
-                });
-            });
+                },
+                error: function() {
 
-            
+                }
+            });
+        });
     </script>
 </body>
 
